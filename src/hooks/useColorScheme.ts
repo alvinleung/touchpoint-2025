8a27a656a445e6cbdde073842d2ec0296e2daa3c
alvin leung <alvinleung2009@gmail.com>
2025-02-22 20:25:54 -0800

    change color scheme switcher implementation

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -1,13 +1,17 @@
-import { useIsomorphicLayoutEffect } from "framer-motion";
+import { useRef } from "react";
 
 export function useColorScheme(name: "red" | "green") {
-  useIsomorphicLayoutEffect(() => {
-    if (name === "red") {
-      document.body.classList.add("color-scheme-red");
-      document.body.classList.remove("color-scheme-green");
-    } else {
-      document.body.classList.add("color-scheme-green");
-      document.body.classList.remove("color-scheme-red");
-    }
-  }, [name]);
+  const hasRun = useRef(false);
+
+  if (hasRun.current) return;
+
+  if (name === "red") {
+    document.body.classList.add("color-scheme-red");
+    document.body.classList.remove("color-scheme-green");
+  } else {
+    document.body.classList.add("color-scheme-green");
+    document.body.classList.remove("color-scheme-red");
+  }
+
+  hasRun.current = true;
 }
