ff66380037e33ef6f1b93327686958bdbf33f47f
alvin leung <alvinleung2009@gmail.com>
2025-03-16 23:23:42 -0700

    NUKE LAUNCH YET AGAIN

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -28,8 +28,8 @@ const Schedule = () => {
                 )}
                 {item.type === "talk" && (
                   <>
-                    <span className="font-bold">{item.event}</span>,{" "}
-                    <span className="font-light">{item.affiliation}</span>
+                    <span className="font-bold">{item.event}</span>
+                    <span className="font-light">, {item.affiliation}</span>
                   </>
                 )}
               </div>
