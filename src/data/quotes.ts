e50830d6c3d386490408bfb074d554249eb8e5e6
notiennv <notjennynguyen@gmail.com>
2025-03-16 23:53:24 -0700

    Update quotes p2.ts

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -2,7 +2,8 @@ const QUOTES = [
   {
     quote: "How many designers does it take to change a lightbulb?",
     author: "Unknown",
-  },{
+  },
+  {
     quote: "Don't design in your heads!",
     author: "Russell Taylor",
   },
@@ -41,20 +42,94 @@ const QUOTES = [
     author: "Albert Einstein",
   },
   {
+    quote: "We don’t have time to get all emotional about our failures, all we see is the challenge before us to get it right, and not make that mistake again. And eventually, if you do that enough: it WILL start to become easy, or easier.",
+    author: "Russell Taylor",
+  },
+  {
     quote: "The quality of your process, will determine the quality of your results.",
     author: "Clement Mok",
   },
+  {
+    quote: "Designing is first off about THINKING. Before you design anything, think first.",
+    author: "Russell Taylor",
+  },
+  {
+    quote: "As you design: remember this bigger picture. And you continue as a designer, be in the practice of continuing to ask: what is design? What is good design? And, why do we design? As well as the seemingly easier question of whom are we designing for. Because if we don’t, I fear that “design” as UX design, will lose its very soul.",
+    author: "Russell Taylor",
+  }, 
   {
     quote: "Do not think of your faults, still less the faults of others; look for what is good and strong, and try to imitate it. Your faults will drop off, like dead leaves, when their time comes.",
     author: "John Ruskin",
   },
   {
+    quote: "There are two things deadly to a student design project team process: laptops up and thinking in words.",
+    author: "Russell Taylor",
+  }, 
+  {
+    quote: "The most important thing about effective process is to keep it moving. Good process should feel like you are active, in motion.",
+    author: "Russell Taylor",
+  }, 
+  {
+    quote: "When you begin to question your surroundings and find out how to solve the problems that you see, you start to become a designer.",
+    author: "Inga Sempe",
+  }, 
+  {
+    quote: "Make sure that your motive is the love of each other, much more than the THINGS we create.",
+    author: "Russell Taylor",
+  }, 
+  {
+    quote: "Isn’t all design centered around users? No. In fact, the forces that drive product development range from short-term economic interests of manufacturers to the expressive or theoretical intent of designers, to a community’s entrenched habits and customs. Sometimes things look the way they do because that’s the cheapest and fastest way to make them, sometimes because that’s the way things have always been done.",
+    author: "Ellen Lupton",
+  },
+  {
+    quote: "When I am working on a problem I never think about beauty. I only think about how to solve the problem. But when I have finished, if the solution is not beautiful, I know it is wrong.",
+    author: "Buckminster Fuller",
+  },
+  {
+    quote: "I have faith in you. I have faith that any rough edges can be worn off.",
+    author: "Russell Taylor",
+  },
+  {
+    quote: "You probably have heard the saying, 'it's important to care, but be careful of caring too much'.  When we over-invest, our egos can get in the way. and, as our work is so collective, we simply cannot let things go there.",
+    author: "Russell Taylor",
+  },
+  {
+    quote: "When designers create products, spaces or media they ask along the way how human beings will interact with their work. Engineers start from technology and look for a use for it; business people start with a business proposition and then look for the technology and people. Designers start with people, coming towards a solution from the point of view of people.",
+    author: "Bill Moggridge",
+  },
+  {
+    quote: "IN THE ROOM: we are here to learn, to inspire each other, to inspire ourselves, to be changed by the learning, and what we are learning. The walls of the classroom are for me sacred. When we enter that space, it is a context, worthy of our deep respect, and a container for our hopes, dreams, goals and ambitions collectively.",
+    author: "Russell Taylor",
+  },
+  {
+    quote: "I take my work seriously and I expect you to also. The work is serious. The play is also serious. But we play AFTER we work. The work is never funny, or light.",
+    author: "Russell Taylor",
+  },
+  {
+    quote: "Design makes what is hard, easier, and what is easy, memorable.",
+    author: "John Maeda",
+  },
+  {
+    quote: "If they boo ya, you gotta make em pay.",
+    author: "Satchel Paige",
+  },
+   {
     quote: "Give it all away.",
+    author: "Russell Taylor",
+  }, 
+  {
+    quote: "But all shall be well and / All manner of thing shall be well / By the purification of the motive / In the ground of our beseeching.",
+    author: "T.S. Eliot",
+  },
+  {
+    quote: "Étonnez moi.",
+    author: "Alexey Brodovitch",
+  },
+  {
+    quote: "Give a shit.",
     author: "Russell Taylor",
   },
  
-
-
 ];
 
 export default QUOTES;
